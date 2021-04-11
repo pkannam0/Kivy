79a7f18507cb6e4d9b6d1d7982f5f6488af30563
AlphaRacer <tarun.a1441@gmail.com>
2021-04-11 16:05:32 +0530

    Fix small sytax problems

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 
+
 class Work(FloatLayout):
-        pass
+    pass
+
 
 class My_AppApp(App):
     def build(self):
         return Work()
+
 
 if __name__ == '__main__':
     My_AppApp().run()
