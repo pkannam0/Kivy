14a6a198b296abc0a3f4c6419c2638ef9132e26c
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2021-04-11 17:06:47 +0530

    Created Login Screen

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ class My_AppApp(MDApp):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "BlueGray"
         return Builder.load_file("my_app.kv")
+    
+    def sign_up(self):
+        self.root.ids.email.text = ""
+        self.root.ids.password.text = ""
+        self.root.ids.welcome_label.text = f'Hey! Welcome to the LFL App'
+
+    def login(self):
+        self.root.ids.email.text = ""
+        self.root.ids.password.text = ""
+        self.root.ids.welcome_label.text = f'Welcome Back!'
 
 
 
